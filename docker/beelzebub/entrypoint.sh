92f645f3526f16d2dcf8b0b5450c0ee6dc99fc98
guruh-edi <guruh.edi@opsnow.com>
2025-10-16 08:39:45 +0700

    eval print

--- docker/beelzebub/entrypoint.sh
+++ docker/beelzebub/entrypoint.sh
@@ -3,7 +3,7 @@
 ACTUAL_ROOT=/opt/honeypot/actual-root
 MOUNT_PATH=/opt/honeypot/mnt
 
-echo whoami
+echo $(whoami)
 
 mkdir -p $ACTUAL_ROOT $MOUNT_PATH
 rsync -a /bin /lib /usr/bin /etc $ACTUAL_ROOT
